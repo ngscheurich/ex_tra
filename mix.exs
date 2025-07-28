406b20251d7fdd2658c81d1fe71aa8e4f72c9818
Jason Johnson <jasonmj@gmail.com>
2025-07-28 09:53:56 -0400

    Add vim plugin

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExTra.MixProject do
   def project do
     [
       app: :ex_tra,
-      aliases: [build: ["escript.build", "cmd cp extra vscode/out"]],
+      aliases: [build: ["escript.build", "cmd cp extra vim", "cmd cp extra vscode/out"]],
       version: "0.1.0",
       elixir: "~> 1.17",
       elixirc_paths: if(Mix.env() == :test, do: ["lib", "test/support"], else: ["lib"]),
